90d519129eb3c1aa63bba5c7d6fca06ccdc861f3
thomas hamer <thomas.hamer@cmd.com.au>
2021-07-20 19:56:04 +1000

    update vars

--- vars.tf
+++ vars.tf
@@ -102,7 +102,7 @@ variable "rejected_patches" {
 variable "default_baselines" {
   description = "list of baselines for index to propagate"
   type        = list(any)
-  default     = element ([
+  default     = [
     data.aws_ssm_patch_baseline.windows.id,
     data.aws_ssm_patch_baseline.suse.id,
     data.aws_ssm_patch_baseline.ubuntu.id,
@@ -112,7 +112,7 @@ variable "default_baselines" {
     data.aws_ssm_patch_baseline.amazon2.id,
     data.aws_ssm_patch_baseline.centos.id,
     data.aws_ssm_patch_baseline.oracle.id
-  ], count.index)
+  ]
 }
 
 variable "custom_baselines" {
