41efe030a245980d41d5919fb7c77e073a29ea76
thomas hamer <thomas.hamer@cmd.com.au>
2021-07-20 20:07:46 +1000

    rollback

--- patch_groups.tf
+++ patch_groups.tf
@@ -2,7 +2,7 @@
 ########## SCAN ##########
 resource "aws_ssm_patch_group" "patchgroup-scan-default" {
   count = 9
-  baseline_id =  baseline_id =  element ([
+  baseline_id = element ([
     data.aws_ssm_patch_baseline.windows.id,
     data.aws_ssm_patch_baseline.suse.id,
     data.aws_ssm_patch_baseline.ubuntu.id,
